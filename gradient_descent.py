c6b36bf4841cb34d561d3fe5264110cf7f613a5a
Ilya Frolov <fiss205@gmail.com>
2020-01-25 23:18:03 +0300

    Regularized linear regression. Bias/variance.

--- gradient_descent.py
+++ gradient_descent.py
@@ -9,18 +9,18 @@ def gradient_descent(coefficients_count: int,
     """
     Calculate regression coefficients in vector form
 
-    @param coefficients_count: regression coefficients count
-    @param hypothesis_function_derivative: first derivative dJ/dCj of hypothesis function J(C)
-    @param args: hypothesis function derivative arguments
-    @param learning_rate: gradient descent alpha parameter
-    @param max_iterations: max algorithm iterations until convergence
-    @return: n x 1 vector of linear coefficients
+    :param coefficients_count: regression coefficients count
+    :param hypothesis_function_derivative: first derivative dJ/dCj of hypothesis function J(C)
+    :param args: hypothesis function derivative arguments
+    :param learning_rate: gradient descent alpha parameter
+    :param max_iterations: max algorithm iterations until convergence
+    :return: 1 x n vector of linear coefficients
     """
-    # n x 1 vector
-    coefficients = np.zeros(coefficients_count)
+    # 1 x n vector
+    coefficients = np.zeros((1, coefficients_count))
 
     for i in range(max_iterations):
         coefficients = coefficients - learning_rate \
                        * hypothesis_function_derivative(coefficients, *args)
 
-    return coefficients
+    return coefficients[0]
--- linear_regression.py
+++ linear_regression.py
@@ -5,43 +5,59 @@ def cost_function(coefficients, x, y):
     """
     Calculate cost function for linear regression
 
-    @param coefficients: (1 x n) vector of regression coefficients
-    @param x: (m x n) matrix, m - experiments count, n - variables count
-    @param y: (m x 1) results vector
-    @return: summary cost
+    :param coefficients: (1 x n) vector of regression coefficients
+    :param x: (m x n) matrix, m - experiments count, n - variables count
+    :param y: (m x 1) results vector
+    :return: summary cost
     """
     coefficients = coefficients.reshape(coefficients.size, 1)
     diff = (x @ coefficients - y)
-    return 1 / (2 * y.size) * (diff.transpose() @ diff)[0, 0]
+    return 1 / (2 * y.shape[0]) * (diff.transpose() @ diff)[0, 0]
+
+
+def regularized_cost_function(coefficients, x, y, regularization_rate):
+    c = np.reshape(coefficients[1:coefficients.size], (coefficients.size - 1, 1))
+
+    return cost_function(coefficients, x, y) \
+           + regularization_rate / (2 * y.shape[0]) * (c.transpose() @ c)[0, 0]
 
 
 def cost_function_derivative(coefficients, x, y):
     """
-    @param x: input variables (m x (n - 1)) matrix,
-              n - variables count, m - experiments count
-    @param y: output results (m x 1) vector, m - experiments count
-    @return: (n x 1) vector of derivative calculation results,
-             n - variables (coefficients) count
-    """
-    coefficients = coefficients.reshape(coefficients.size, 1)
+    Calculate linear regression cost function derivative dJ/dC for each coefficient
 
-    return np.array([1 / y.size * np.dot(
+    :param coefficients: (1 x n) vector of regression coefficients
+    :param x: input variables (m x n) matrix,
+              n - variables count, m - experiments count
+    :param y: output results (m x 1) vector, m - experiments count
+    :return: (1 x n) vector of derivative calculation results
+    """
+    coefficients = coefficients.reshape(coefficients.size, 1)  # n x 1
+
+    return 1 / y.shape[0] * np.dot(
         np.subtract(
             x @ coefficients,
             y
         ).transpose(),  # 1 x m
-        x[:, index:(index + 1)]  # m x 1
-    )[0, 0] for index in range(coefficients.size)])
+        x  # m x n
+    )[0]  # 1 x n
+
+
+def regularized_cost_function_derivative(coefficients, x, y, regularization_rate):
+    c = np.concatenate([[0], coefficients[1:coefficients.size]])
+
+    return cost_function_derivative(coefficients, x, y) \
+           + regularization_rate / y.shape[0] * c
 
 
 def normal_equation(x, y):
     """
     Calculate linear regression coefficients analytically using normal equation
 
-    @param x: input variables (m x (n - 1)) DataFrame,
+    :param x: input variables (m x (n - 1)) matrix,
               n - variables count, m - experiments count
-    @param y: output results (m x 1) DataFrame, m - experiments count
-    @return: n x 1 vector of linear coefficients
+    :param y: output results (m x 1) vector, m - experiments count
+    :return: 1 x n vector of linear coefficients
     """
     coefficients = np.linalg.pinv(x.transpose() @ x) @ x.transpose() @ y
     return coefficients.transpose()[0]
